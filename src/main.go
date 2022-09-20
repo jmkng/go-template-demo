32d41a16ceacbd40882d5cf3b6148f8b4e8b769e
Jon Koenig <jmkoeni@icloud.com>
2022-09-20 17:40:20 -0600

    Added note about template override

--- src/main.go
+++ src/main.go
@@ -10,8 +10,10 @@ func main() {
 	// Parse "common" templates
 	tpl := template.Must(template.ParseGlob("templates/common/*.tmpl"))
 
-	// Parse the "target" template
+	// Parse the "target" template. Comment one and uncomment the other to see how
+	// the "content" block in base.tmpl is overriden based on which file you parse.
 	tpl.ParseFiles("templates/index.tmpl")
+	// tpl.ParseFiles("templates/about.tmpl")
 
 	// You can see the template map for debugging purposes.
 	fmt.Println(tpl.DefinedTemplates())
